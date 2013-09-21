fabbfae31b7dfd09340eb1263927fc688c649b15
Brandon DeRosier <btd@cheesekeg.com>
2013-09-21 05:00:55 -0400

    Hanoi visuals work

--- hanoi/hanoi.js
+++ hanoi/hanoi.js
@@ -1,17 +1,26 @@
 // Global objects
 
-var stage;
+var stage, hanoi;
 
 // Entry point
 
 function start() {
     stage = new Stage('c');
-    
+
+    hanoi = new Hanoi();
+    stage.addChild(hanoi);
+    centerHanoi();
+    stage.addEventListener(Event.RESIZE, centerHanoi);
 }
 
 // Misc functions
 
 function color(r, g, b) { return ((r << 8) + g << 8) + b }
+
+function centerHanoi() {
+    hanoi.x = stage.stageWidth/2;
+    hanoi.y = stage.stageHeight/1.5;
+}
 
 // Array extensions
 
@@ -32,8 +41,16 @@ Array.prototype.last = function() {
 // Disk
 
 function Disk(size) {
+    Sprite.call(this);
+
+    this.graphics.beginFill(Math.random()*0xffffff, 0.7);
+    this.graphics.drawRect(-25*size - 20, 0, 50*size + 40, 50);
+    this.graphics.endFill();
+
     this.size = size;
 }
+
+Disk.prototype = new Sprite();
 
 Disk.prototype.isBigger = function(other) {
     return this.size > other.size;
@@ -42,13 +59,21 @@ Disk.prototype.isBigger = function(other) {
 // Peg - holds a stack of disks
 
 function Peg() {
+    Sprite.call(this);
+
+    this.graphics.beginFill(0x555555, 1);
+    this.graphics.drawRect(-20, -360, 40, 420);
+    this.graphics.endFill();
+
     this.stack = new Array();
 }
+
+Peg.prototype = new Sprite();
 
 Peg.prototype.push = function(disk) {
     if (this.stack.contains(disk))
 	console.log("DISK PLACEMENT ERROR: Duplicate disk placed on peg");
-    if (disk.isBigger(this.stack.last()))
+    if (this.stack.length > 0 && disk.isBigger(this.stack.last()))
 	console.log("DISK PLACEMENT ERROR: Disk being placed on peg is larger than the current top disk");
 
     this.stack.push(disk);
@@ -61,14 +86,36 @@ Peg.prototype.pop = function() {
 // Hanoi - holds three pegs and shifts disks between them
 
 function Hanoi() {
+    Sprite.call(this);
+
+    this.graphics.beginFill(0x333333, 1);
+    this.graphics.drawRect(-450, 60, 900, 50);
+    this.graphics.endFill();
+
     this.pegs = [];
 
-    for (var i = 0; i < 3; i++)
-	this.pegs[i] = new Peg();
+    for (var i = 0; i < 3; i++) {
+	var peg = new Peg();
 
-    for (var i = 5; i > 0; i--)
-	this.pegs[0].push(new Disk(i))
+	this.pegs[i] = peg;
+	this.addChild(peg);
+
+	peg.x = i*300 - 300;
+    }
+
+    for (var i = 5; i > 0; i--) {
+	var disk = new Disk(i);
+
+	this.pegs[0].push(new Disk(i));
+	this.addChild(disk);
+
+	var startPosition = this.diskDestination(0, -i + 5);
+	disk.x = startPosition.x;
+	disk.y = startPosition.y;
+    }
 }
+
+Hanoi.prototype = new Sprite();
 
 Hanoi.prototype.moveDisk = function(source, destination) {
     if (!this.pegs[source].length) {
@@ -76,5 +123,13 @@ Hanoi.prototype.moveDisk = function(source, destination) {
 	return false;
     }
 
-    this.pegs[destination].push(this.pegs[source].pop());
+    var sourcePeg = this.pegs[source].pop();
+    this.pegs[destination].push(sourcePeg);
+
+    var moveTo = this.diskDestination(destination, this.pegs[destination].length - 1);
+    Tweener.addTween(sourcePeg, {x: moveTo.x, y: moveTo.y, transition: 'easeInOutQuad', time: 1});
+}
+
+Hanoi.prototype.diskDestination = function(peg, level) {
+    return {x: this.pegs[peg].x, y: -level*60}
 }
